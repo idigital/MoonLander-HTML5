ad4732d75a703c6aefbfe784d197db4cb5d4ec69
Seb Lee-Delisle <sebleedelisle@gmail.com>
2012-02-21 18:53:31 +0000

    first commit

---
+++ KeyTracker.js
@@ -0,0 +1,20 @@
+KeyTracker = new (function(){
+	
+	this.keysPressed = {}; 
+	this.UP = 38; 
+	this.LEFT = 37; 
+	this.RIGHT = 39; 
+	this.DOWN = 40; 
+	
+	this.isKeyDown = function (key) { 
+		if (typeof key == 'string')
+			key = key.charCodeAt(0); 
+		return(this.keysPressed[key]);
+	};
+	
+	document.addEventListener("keydown", function(e) {	
+		KeyTracker.keysPressed[e.keyCode] = true; }); 
+	document.addEventListener("keyup", 	function(e) {
+		KeyTracker.keysPressed[e.keyCode] = false;}); 
+		
+})();
